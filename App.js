bf22069a8ff675886749206310dc070d728d47dc
jjhiggz <jonathan.higger@gmail.com>
2021-01-26 13:00:37 -0700

    first commit

---
+++ App.js
@@ -0,0 +1,43 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <input placeholder="start typing number" type="text" />
+      <div className="number-properties-container">
+        <div className="number-property">
+          <h3>isEven: {"true"}</h3>
+        </div>
+        <div className="number-property">
+          <h3>isEven: {"true"}</h3>
+        </div>
+        <div className="number-property">
+          <h3>isEven: {"true"}</h3>
+        </div>
+        <div className="number-property">
+          <h3>isEven: {"true"}</h3>
+        </div>
+        <div className="number-property">
+          <h3>isEven: {"true"}</h3>
+        </div>
+        <div className="number-property">
+          <h3>isEven: {"true"}</h3>
+        </div>
+        <div className="number-property">
+          <h3>isEven: {"true"}</h3>
+        </div>
+        <div className="number-property">
+          <h3>isEven: {"true"}</h3>
+        </div>
+        <div className="number-property">
+          <h3>isEven: {"true"}</h3>
+        </div>
+        <div className="number-property">
+          <h3>isEven: {"true"}</h3>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
